425106361bc045a9427f03a445d30ace588966e1
Collin Rickford <32271925+crickford@users.noreply.github.com>
2024-07-01 14:06:19 -0700

    add missing commands and tests

--- src/actions/rectangle-pro.test.ts
+++ src/actions/rectangle-pro.test.ts
@@ -2,10 +2,34 @@ import { expect, test } from "vitest";
 import { commandGroups, actions } from "./rectangle-pro";
 
 test("All actions are supported", () => {
-  const allActions = Object.values(commandGroups)
+  const supportedActions = Object.values(commandGroups)
     .map((group) => group.items)
     .flat()
     .map((item) => item.name);
 
-    expect(allActions.sort()).toContainEqual(actions.toSorted());
+  expect(supportedActions.toSorted()).toEqual(actions.toSorted());
+});
+
+test("All actions are unique", () => {
+  const supportedActions = Object.values(commandGroups)
+    .map((group) => group.items)
+    .flat()
+    .map((item) => item.name);
+
+  const counts = new Map();
+
+  for (const action of supportedActions) {
+    if (counts.has(action)) {
+      counts.set(action, counts.get(action)! + 1);
+    } else {
+      counts.set(action, 1);
+    }
+  }
+
+  // filter out actions that are not unique
+  const nonUnique = Array.from(counts)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    .filter(([_, count]) => count > 1);
+
+  expect(nonUnique).toEqual([]);
 });
